e54c57f70120b2565f919dc391ce4c581f58afcb
jairocket <jailsonanjos@hotmail.com>
2021-09-08 22:05:26 -0300

    create page characters

--- marvel/src/App.js
+++ marvel/src/App.js
@@ -1,32 +1,29 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-// import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import api from './services/api'
-import hash from './services/hash'
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+// import api from './services/api'
+// import hash from './services/hash'
 function App() {
-  function handleButton(){
-    fetch(api+'/characters'+hash).then(
-      res => res.json()
-    ).then(res => console.log(res.data.results))
-  }
+  // function handleButton(){
+  //   fetch(api+'/characters'+hash).then(
+  //     res => res.json()
+  //   ).then(res => console.log(res.data.results))
+  // }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <button onClick ={handleButton}>Click me</button>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path='/characters'>
+
+        </Route>
+        <Route exact path ='/'>
+
+        </Route>
+        <Route exact path='/not-found' />
+       
+        <Redirect to='/not-found'/>
+      </Switch>
+    </BrowserRouter>
+    
   );
 }
 
---
+++ marvel/src/pages/characters/index.js
@@ -0,0 +1,10 @@
+
+
+function Characters(){
+    return(
+        <>
+        </>
+    )
+}
+
+export default Characters;
