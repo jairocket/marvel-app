071a1b41341b408a2be9f919f580a8d54b6da703
jairocket <jailsonanjos@hotmail.com>
2021-09-20 23:29:09 -0300

    create infinite scrolling part 1

--- marvel/src/pages/characters/index.tsx
+++ marvel/src/pages/characters/index.tsx
@@ -1,7 +1,7 @@
 import '../../pages/characters/styles.css'
 import '../../styles/global.css'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef, useCallback } from 'react'
 
 import api from "../../services/api"
 import hash from '../../services/hash'
@@ -18,26 +18,37 @@ interface CharacterResponseProps{
 
 function Characters(){
 
-    const [characters, setCharacters] = useState<CharacterResponseProps[]>([])
+    const [characters, setCharacters] = useState<CharacterResponseProps[]>([]);
+    const [offset, setOffset] = useState(0);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+    const [hasmore, setHasMore] = useState(false);
+    const observer = useRef()
 
     useEffect(()=>{
-        fetch(api+'/characters'+hash)
+        setLoading(true);
+        setError(false);
+        fetch(`${api}/characters?limit=20&${hash}&offset=${offset}`)
         .then(res => res.json())
-        .then(res=> setCharacters(res.data.results))
-    },[])
-
-    // if(!characters) return null
+        .then(res=> {
+            setCharacters([...characters, ...res.data.results]);
+            setHasMore(res.data.results.length > 0);
+            setLoading(false);
+        })
+    },[offset]);
 
 
     return(
 
         <div className='characters'>
             {characters.map((item)=>
-                <div className='characters-character'>
+                <div className='characters-character' key={item.name}>
                     <h1>{item.name}</h1>
                     <Image image={item.thumbnail}/>
                 </div>
             )}
+            <div>{loading && 'Loading...'}</div>
+            <div>{error && 'Error'}</div>
         </div>
         
     )
